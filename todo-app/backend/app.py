186032d2dd5975ff41e1a2b320bf4c12a1c2023d
Ranga <ngjulian@gmail.com>
2025-04-08 17:01:28 +0800

    ....

--- todo-app/backend/app.py
+++ todo-app/backend/app.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://urban-space-sniffle-5gq79q9prwq5c4rwq-5173.app.github.dev"}})
-
+# CORS(app, resources={r"/*": {"origins": "https://urban-space-sniffle-5gq79q9prwq5c4rwq-5173.app.github.dev"}})
+# CORS(app, resources={r"/*": {"origins": "http://localhost:5173/"}})
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
